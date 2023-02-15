a60906af800c36c1e0b2ef2bb1819c93e614cf70
Alec-Blake <blakey602@gmail.com>
2023-02-14 19:44:43 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+///////////////////
+////MODULE-24.3////
+///////////////////
+
+/*
 console.log(document);
 
 // querySelector examples
@@ -46,13 +51,12 @@ for (i = 0; i < divAreasList.length; i++) {
     console.log(element);
 }
 
-/*
-Very old browsers (and Internet Explorer in particular)
-don't fully support the NodeList interface. As such, the 
-code above may not work as intended. If you need to support 
-Internet Explorer, you can use the Array.prototype.forEach() method, 
-like this: 
-*/
+// Very old browsers (and Internet Explorer in particular)
+// don't fully support the NodeList interface. As such, the 
+// code above may not work as intended. If you need to support 
+// Internet Explorer, you can use the Array.prototype.forEach() method, 
+// like this: 
+
 
 // Get a list of descriptions
 const list = document.querySelectorAll(".description-display");
@@ -61,3 +65,77 @@ const list = document.querySelectorAll(".description-display");
 Array.prototype.forEach.call(list, function (element) {
   console.log(element);
 });
+*/
+
+///////////////////
+////MODULE-24.4////
+///////////////////
+const descriptions = document.querySelectorAll(".description-display");
+
+for (let desc of descriptions.values()) {
+  let content = desc.innerText;
+
+  if (content.length > 250) {
+    content = content.slice(0, 250);
+    content = content + '<a href="#">...</a>';
+  }
+
+  desc.innerHTML = content;
+}
+
+const ratings = document.querySelectorAll(".rating-display .value");
+
+for (let rating of ratings) {
+  let ratingValue = parseFloat(rating.innerText);
+
+  if (ratingValue > 4.7) {
+    rating.classList.add("high-rating");
+
+    rating.classList.remove("value");
+  }
+}
+
+// Suppose that you wanted to dynamically add a statement to the page's
+// heading, stating the number of parks on display.
+
+// First, select the parks on the page using the park class as a selector:
+const parks = document.querySelectorAll(".park-display");
+
+// Then, get the number of parks using the length property of the list:
+const numberParks = parks.length;
+
+// Next, create a new element. In this case, you should use a <div>:
+const newElement = document.createElement("div");
+
+/*
+This creates an empty element. But, you have access to 
+all the properties and methods that were used above 
+to modify an element. You can set the text of this element 
+with the innerText property, like this: 
+*/
+newElement.innerText = `${numberParks} exciting parks to visit`;
+
+/*
+You can also style this new element with either the 
+style property or the classList property. Create a new 
+CSS rule for the class header-statement in the style.css file 
+*/
+newElement.classList.add("header-statement");
+
+/*
+Finally, to add the new element to the DOM, select the <header> 
+element and use the appendChild() method to add the new element 
+to the <header>, as shown below.
+*/
+const header = document.querySelector("header");
+header.appendChild(newElement);
+
+// Removing DOM elements
+// Get the parent element of all parks
+const main = document.querySelector("main");
+
+// Select a single park
+const park = main.querySelector(".park-display");
+
+// Remove that park
+// main.removeChild(park);
